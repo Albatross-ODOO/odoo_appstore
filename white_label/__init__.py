e69b3e1d6506a2ff10978d84c05a73a88035e93c
hardikanavadia <hardikanavadia>
2026-08-12 13:27:59 +0530

    v17 base ref

---
+++ white_label/__init__.py
@@ -0,0 +1,13 @@
+from . import controllers
+from . import models
+from odoo.exceptions import ValidationError
+
+
+def pre_init_check(cr):
+    from odoo.service import common
+    version_info = common.exp_version()
+    server_serie = version_info.get('server_serie')
+    if server_serie != '17.0': raise ValidationError(
+        'Module support Odoo series 17.0, found {}, Consider acquiring the appropriate version through a purchase if necessary..'.format(
+            server_serie))
+    return True
